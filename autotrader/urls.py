f1ac9fa3a12de723d8bf6a7149681236726b91c0
ilyasolgun11 <127804888+ilyasolgun11@users.noreply.github.com>
2024-07-22 17:06:43 +0100

    Create home view

--- autotrader/urls.py
+++ autotrader/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from base import views as base_views
 
 urlpatterns = [
+    path("home/", base_views.home, name='home'),
     path('admin/', admin.site.urls),
 ]
